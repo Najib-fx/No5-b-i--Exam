5c02d4cd98061d8f87be811a4eacd5081da1b968
Najib <61076460+Najib-fx@users.noreply.github.com>
2024-09-22 12:28:57 +0300

    Add files via upload

---
+++ No5(b)(i)-Exam/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string enterName;
+   //Enter the name
+   cout << "Enter your name " << endl;
+   cin >> enterName;
+    // Print out your name
+    cout << "Your Name is "<< enterName << endl;
+    return 0;
+}
